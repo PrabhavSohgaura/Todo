c3c7bf626ba6234cc2c392134be714ae3ad6d8b4
PrabhavSohgaura <prabhavsohgaura333@gmail.com>
2023-11-20 07:34:58 +0530

    added the input for todo

--- client/src/components/Todo/TodoInput.jsx
+++ client/src/components/Todo/TodoInput.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const TodoInput = () => {
-  return <div>TodoInput</div>;
+  return (
+    <div>
+      <h3>First todo</h3>
+    </div>
+  );
 };
 
 export default TodoInput;
