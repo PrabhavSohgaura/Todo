92621f8d5e115e88bd5eaab27a78339fcd7523f5
PrabhavSohgaura <prabhavsohgaura333@gmail.com>
2023-11-23 09:14:47 +0530

    added todo

--- client/src/components/Todo/TodoInput.jsx
+++ client/src/components/Todo/TodoInput.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TodoInput = ({ todo }) => {
-  return <div className="container">{todo}</div>;
+  return <div className="todo__Container">{todo}</div>;
 };
 
 export default TodoInput;
