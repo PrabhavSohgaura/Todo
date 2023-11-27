d86b3835a812c8374e6cd68a4a1c7e1d009a8300
PrabhavSohgaura <prabhavsohgaura333@gmail.com>
2023-11-27 09:39:09 +0530

    added the btn

--- client/src/components/Todo/TodoInput.jsx
+++ client/src/components/Todo/TodoInput.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const TodoInput = ({ todo }) => {
-  return <div className="todo__Container">{todo}</div>;
+  return (
+    <div className="todo__Container">
+      <h1>{todo}</h1>
+      <button>Delete</button>
+    </div>
+  );
 };
 
 export default TodoInput;
